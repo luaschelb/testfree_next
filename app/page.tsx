8ce9f7286af0d9df97292e9153f00544d4cae596
Luana Schelb <lua.schelb@gmail.com>
2025-01-26 14:00:30 -0300

    Deleted other files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import {fetchBuilds} from "./lib/data"
 
-export default function Home() {
+export default async function Home() {
+  const builds = await fetchBuilds();
+
   return (
     <div className="container">
       <h1>HOMEPAGE</h1>
       <Link href="/projetos">LINK Projetos</Link>
+      {
+        builds.map((build) => (<p key={build.id}>{build.title}</p>))
+      }
     </div>
   );
 }
