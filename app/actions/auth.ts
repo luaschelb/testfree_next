c78f2848df6b5badb130c3ce8b1c1aecd6f02f15
Luana Schelb <lua.schelb@gmail.com>
2025-03-10 21:26:17 -0300

    advances in the signup action and form validation

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,3 +1,41 @@
 'use server'
  
-export async function signup() {}
+import { z } from 'zod'
+
+const signUpFormSchema = z.object({
+    name: z.string().min(2, {message: "Must be at least 2 characters"}),
+    email: z.string().email({message: "Invalid email"}),
+    password: z
+        .string()
+        .min(8, {message: "Be at least 8 characters long"})
+        .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+        .regex(/[0-9]/, { message: 'Contain at least one number.' })
+        .regex(/[^a-zA-Z0-9]/, {
+            message: 'Contain at least one special character.',
+        })
+})
+
+export type FormState =
+  | {
+      errors?: {
+        name?: string[]
+        email?: string[]
+        password?: string[]
+      }
+      message?: string
+    }
+  | undefined
+
+export async function signup(state: FormState, formData : FormData) {
+    const validatedFields = signUpFormSchema.safeParse({
+        name: formData.get("name"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+    })
+
+    if (!validatedFields.success) {
+        return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,25 +1,43 @@
+'use client'
 import { signup } from '@/app/actions/auth'
+import { useActionState } from 'react'
  
 export default function SignupForm() {
+  const [state, action, pending] = useActionState(signup, undefined)
   return (
     <div className='w-fit m-auto mt-[15%]'>
-      <form action={signup} className="border rounded p-2">
-        <h1 className='text-center font-bold'>Register</h1>
+      <form action={action} className="border rounded p-2">
+        <h1 className='text-center font-bold'>Sign Up</h1>
         <div className='mb-2'>
           <label className="block" htmlFor="name">Name</label>
           <input id="name" name="name" placeholder="Name" className="py-2 px-3 shadow border rounded"/>
+          {state?.errors?.name && <p>{state.errors.name}</p>}
         </div>
         <div className='mb-2'>
           <label className="block" htmlFor="email">Email</label>
           <input id="email" name="email" type="email" placeholder="Email" className="py-2 px-3 shadow border rounded"/>
+          {state?.errors?.email && <p>{state.errors.email}</p>}
         </div>
         <div className='mb-2'>
           <label className="block" htmlFor="password">Password</label>
           <input id="password" name="password" type="password" 
           className="py-2 px-3 shadow border rounded"/>
+          {state?.errors?.password && (
+            <div>
+              <p>Password must:</p>
+              <ul>
+                {
+                  state.errors.password.map((item) => (
+                    <li key={item}>{item}</li>
+                  ))
+                }
+              </ul>
+            </div>
+          )}
         </div>
-        <button type="button" 
-          className="py-2 px-3 shadow border rounded bg-teal-100 hover:bg-teal-200 p-1 hover:cursor-pointer">Sign Up</button>
+        <button type="submit" 
+        disabled={pending}
+          className="py-2 px-3 shadow border rounded bg-teal-100 hover:bg-teal-200 p-1 hover:cursor-pointer">Register</button>
       </form>
     </div>
   )
