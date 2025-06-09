5a73a145438c34b18a33999f71fe05b9f79d3f82
luaschelb <lua.schelb@gmail.com>
2025-06-09 19:46:34 -0300

    Added placeholder content to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default async function Home() {
   return (
     <main className="grid gap-2 ">
+      <h1 className="text-2xl font-bold">Home Page</h1>
+      <div className="">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis ut dolor non tristique. Cras posuere tincidunt arcu fermentum suscipit. Proin vehicula tempus interdum. Aliquam tincidunt luctus enim, sit amet finibus lectus fermentum et. Ut gravida orci in mauris egestas, non ultrices nibh ultrices. In sapien dui, vestibulum quis eros eu, ultricies vestibulum nibh. Proin id rhoncus dolor. Nunc fermentum dolor tincidunt bibendum malesuada.
+          Ut a mi tellus. Duis aliquet, magna ac vulputate fermentum, dolor neque ultrices ipsum, nec bibendum nisl enim vel ligula. Maecenas lectus arcu, sollicitudin quis tristique at, consectetur eget purus. Cras at vestibulum massa, ac fringilla massa. Fusce condimentum risus ut eros malesuada, quis dignissim mauris porttitor. Aliquam convallis mattis ligula, in commodo quam interdum et. Nam vel varius dui. Duis congue odio orci.
+      </div>
     </main>
   );
 }
